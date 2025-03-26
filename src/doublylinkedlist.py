799616e6a30ec98a8e1bdd79855553afe8760775
Tim Sanders <sanders.timothy.j@gmail.com>
2025-03-25 20:58:12 -0700

    Adding insert_after and remove methods

--- src/doublylinkedlist.py
+++ src/doublylinkedlist.py
@@ -4,7 +4,7 @@ Implements a DoublyLinkedList and Node class to demonstrate doubly-linked lists.
 
 class Node:
     """
-
+    The Node class implements a list node with three attributes, `data`, `next`, and `previous`.
     """
     def __init__(self, initial_data: int):
         self.data = initial_data
@@ -13,7 +13,18 @@ class Node:
 
 class LinkedList:
     """
+    The LinkedList class implements a doubly-linked list data structure with two attributes, `head` and `tail`.
 
+    Methods
+    -------
+    append(new_node: Node)
+        - Add the given node to the end of the list.
+    prepend(new_node: Node)
+        - Add the given node at the beginning of the list.
+    insert_after(current_node: Node, new_node: Node)
+        - Insert a new node after the given current node.
+    remove(current_node: Node)
+        - Remove the given node from the LinkedList instance.
     """
     def __init__(self):
         self.head = None
@@ -36,7 +47,7 @@ class LinkedList:
             self.tail = new_node
         else:
             self.tail.next = new_node
-            new_node.prev = self.tail
+            new_node.previous = self.tail
             self.tail = new_node
 
     def prepend(self, new_node: Node) -> None:
@@ -56,8 +67,62 @@ class LinkedList:
             self.tail = new_node
         else:
             new_node.next = self.head
-            self.head.prev = new_node
+            self.head.previous = new_node
             self.head = new_node
+
+    def insert_after(self, current_node: Node, new_node: Node) -> None:
+        """
+        Insert a new node after the given current node.
+
+        Parameters
+        ----------
+        current_node : Node
+        new_node : Node
+
+        Returns
+        -------
+        None
+        """
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        elif current_node is self.tail:
+            self.tail.next = new_node
+            new_node.previous = self.tail
+            self.tail = new_node
+        else:
+            successor_node = current_node.next
+            new_node.next = successor_node
+            new_node.previous = current_node
+            current_node.next = new_node
+            successor_node.previous = new_node
+
+    def remove(self, current_node: Node) -> None:
+        """
+        Remove the given node from the LinkedList instance.
+
+        Parameters
+        ----------
+        current_node : Node
+
+        Returns
+        -------
+        None
+        """
+        successor_node = current_node.next
+        predecessor_node = current_node.previous
+
+        if successor_node is not None:
+            successor_node.prev = predecessor_node
+
+        if predecessor_node is not None:
+            predecessor_node.next = successor_node
+
+        if current_node is self.head:
+            self.head = successor_node
+
+        if current_node is self.tail:
+            self.tail = predecessor_node
 
 
 if __name__ == "__main__":
@@ -72,4 +137,18 @@ if __name__ == "__main__":
     assert linked_list.head.data == 34
     assert linked_list.tail.data == 34
 
-    node_b = Node(15)
+    num_list = LinkedList()
+    node_a = Node(1)
+    node_b = Node(2)
+    node_c = Node(3)
+    node_d = Node(4)
+    node_e = Node(5)
+
+    num_list.append(node_a)
+    num_list.append(node_b)
+    num_list.append(node_c)
+    num_list.prepend(node_d)
+    num_list.insert_after(node_a, node_e)
+    num_list.remove(node_c)
+    assert num_list.head.data == 4
+    assert num_list.tail.data == 2
