857ddc4009d20088398cb3d621256a57990574e4
Tim Sanders <sanders.timothy.j@gmail.com>
2025-03-25 21:43:54 -0700

    Adding insertion_sort() method and assertions to test method

--- src/singlylinkedlist.py
+++ src/singlylinkedlist.py
@@ -125,6 +125,48 @@ class LinkedList:
             if succeeding_node is None:
                 self.tail = current_node
 
+    def insertion_sort(self) -> None:
+        """
+        Sort the LinkedList instance using the insertion sort algorithm.
+
+        Returns
+        -------
+        None
+        """
+        before_current = self.head
+        current_node = self.head.next
+        while current_node is not None:
+            next_node = current_node.next
+            position = self.find_insertion_position(current_node.data)
+            if position == before_current:
+                before_current = current_node
+            else:
+                self.remove_after(before_current)
+                if position is None:
+                    self.prepend(current_node)
+                else:
+                    self.insert_after(position, current_node)
+            current_node = next_node
+
+    def find_insertion_position(self, data_value: int) -> Node:
+        """
+        Return the node prior to the given `data_value`.
+
+        Parameters
+        ----------
+        data_value : int
+
+        Returns
+        -------
+        Node
+        """
+        position_a = None
+        position_b = self.head
+        while (position_b is not None) and (data_value > position_b.data):
+            position_a = position_b
+            position_b = position_b.next
+        return position_a
+
 
 
 if __name__ == "__main__":
@@ -148,3 +190,18 @@ if __name__ == "__main__":
     assert list_test.head.next.data == 10
     assert list_test.head.next.next.data == 95
     list_test.remove_after(node_a)
+
+    num_list = LinkedList()
+    node_a = Node(72)
+    node_b = Node(91)
+    node_c = Node(53)
+    node_d = Node(12)
+
+    num_list.append(node_a)
+    num_list.append(node_b)
+    num_list.append(node_c)
+    num_list.append(node_d)
+    num_list.insertion_sort()
+    assert num_list.head.data == 12
+    assert num_list.tail.data == 91
+    assert num_list.head.next.data == 53
