f1d27deddcc5cd4e2aa3a4e8cd6c7c98d68a84ec
Timothy Sanders <sanders.timothy.j@gmail.com>
2025-04-13 23:50:15 -0700

    Adding shell sort implementation

--- src/sorting.py
+++ src/sorting.py
@@ -47,3 +47,69 @@ def insertion_sort(numbers: list[int | float]) -> None:
         while j > 0 and numbers[j] < numbers[j - 1]:
             numbers[j], numbers[j - 1] = numbers[j - 1], numbers[j]
             j -= 1
+
+
+def insertion_sort_interleaved(numbers: list[int], start_index: int, gap: int) -> None:
+    """
+
+    Parameters
+    ----------
+    numbers
+    start_index
+    gap
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    ValueError
+        If an empty list, a negative start index, a gap value less than one,
+        or a start index greater or equal to the length of the list.
+    """
+    if not numbers or start_index < 0 or gap < 1 or start_index >= len(numbers):
+        raise ValueError("Invalid parameters")
+
+    for i in range(start_index + gap, len(numbers), gap):
+        j = i
+        while (j - gap >= start_index) and (numbers[j] < numbers[j - gap]):
+            numbers[j], numbers[j - gap] = numbers[j - gap], numbers[j]
+            j = j - gap
+
+
+def shell_sort(numbers: list[int], gap_values: list[int]) -> None:
+    """
+    Sort a list of integers in-place using the Shell sort algorithm.
+
+    The algorithm uses a sequence of gap values, which should typically be provided
+    in descending order, with the final gap being 1 to ensure the list is fully sorted.
+
+    Parameters
+    ----------
+    numbers : list[int]
+        A list of integers to be sorted
+    gap_values : list[int]
+        A list of positive integers representing the gap sequence. This must contain at
+        least one gap, and the final gap value should be 1.
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    ValueError
+        If an empty list is provided for the gap values, or if gap values
+        are provided in non-descending order, or do not end with one.
+    """
+    if not numbers:
+        return None
+    if not gap_values:
+        raise ValueError("You must provide gap values")
+    if gap_values != sorted(gap_values, reverse=True) or gap_values[-1] != 1:
+        raise ValueError("Gap values must be in descending order and end with 1")
+
+    for gap_value in gap_values:
+        for i in range(gap_value):
+            insertion_sort_interleaved(numbers, i, gap_value)
