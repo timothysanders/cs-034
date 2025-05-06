34822f1a655a2977dffb5da5230c0693c338d380
Timothy Sanders <sanders.timothy.j@gmail.com>
2025-05-06 12:28:35 -0700

    Added functions for heap_sort

--- src/sorting.py
+++ src/sorting.py
@@ -329,3 +329,42 @@ def radix_sort(numbers: list[int]) -> None:
     negatives.reverse()
     numbers.clear()
     numbers.extend(negatives + non_negatives)
+
+def max_heap_percolate_down(node_index, heap_list, list_size):
+    child_index = 2 * node_index + 1
+    value = heap_list[node_index]
+    while child_index < list_size:
+        # Find the max among the node and all the node's children
+        max_value = value
+        max_index = -1
+        i = 0
+        while i < 2 and i + child_index < list_size:
+            if heap_list[i + child_index] > max_value:
+                max_value = heap_list[i + child_index]
+                max_index = i + child_index
+            i = i + 1
+
+        if max_value == value:
+            return
+
+        # Swap heap_list[node_index] and heap_list[max_index]
+        temp = heap_list[node_index]
+        heap_list[node_index] = heap_list[max_index]
+        heap_list[max_index] = temp
+        node_index = max_index
+        child_index = 2 * node_index + 1
+
+def heap_sort(numbers):
+    i = len(numbers) // 2 - 1
+    while i >= 0:
+        max_heap_percolate_down(i, numbers, len(numbers))
+        i = i - 1
+
+    i = len(numbers) - 1
+    while i > 0:
+        # Swap numbers[0] and numbers[i]
+        temp = numbers[0]
+        numbers[0] = numbers[i]
+        numbers[i] = temp
+        max_heap_percolate_down(0, numbers, i)
+        i = i - 1
